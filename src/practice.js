9dd63b9428fab4e6aa77b8b0efbae8b6ec2e6037
Steven Bull <stevenwbulljr@gmail.com>
2019-09-30 16:49:18 -0500

    finish configuration of knex into application

--- src/practice.js
+++ src/practice.js
@@ -1,6 +1,9 @@
+require('dotenv').config();
 const knex = require('knex');
 
 const knexInstance = knex( {
   client: 'pg',
-  connection: 'postgresq1://dunder_mifflin:dunder_mifflin@localhost/knex-practice'
+  connection: process.env.DB_URL
 });
+
+console.log('knex and driver installed correctly');
