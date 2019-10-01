be0150bb8df7c7053644bbb24988c70ca098af5a
Steven Bull <stevenwbulljr@gmail.com>
2019-10-01 15:24:23 -0500

    add knex instance to test file

--- test/articles-service.spec.js
+++ test/articles-service.spec.js
@@ -1,6 +1,16 @@
 const ArticlesService = require('../src/articles-service');
+const knex = require('knex');
 
 describe('Articles service object', () => {
+  let db;
+
+  before( () => {
+    db = knex({
+      client: 'pg',
+      connection: process.env.TEST_DB_URL
+    });
+  });
+  
   describe('getAllArticles()', () => {
     it('resolves all articles from "blogful_articles" table', () => {
       //test that ArticleService.getAllArticles gets data from table
