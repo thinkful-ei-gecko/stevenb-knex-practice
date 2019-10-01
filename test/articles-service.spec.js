dcd0fcd731c43343dd299dcc7904f9ab8177f03d
Steven Bull <stevenwbulljr@gmail.com>
2019-10-01 15:32:55 -0500

    add dummy data to test file articles-service.spec.js

--- test/articles-service.spec.js
+++ test/articles-service.spec.js
@@ -3,6 +3,20 @@ const knex = require('knex');
 
 describe('Articles service object', () => {
   let db;
+  let testArticles = [
+    {
+      title: 'First test post!',
+      content: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Natus consequuntur deserunt commodi, nobis qui inventore corrupti iusto aliquid debitis unde non.Adipisci, pariatur.Molestiae, libero esse hic adipisci autem neque ?'
+    },
+    {
+      title: 'Second test post!',
+      content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum, exercitationem cupiditate dignissimos est perspiciatis, nobis commodi alias saepe atque facilis labore sequi deleniti. Sint, adipisci facere! Velit temporibus debitis rerum.'
+    },
+    {
+      title: 'Third test post!',
+      content: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Possimus, voluptate? Necessitatibus, reiciendis? Cupiditate totam laborum esse animi ratione ipsa dignissimos laboriosam eos similique cumque. Est nostrum esse porro id quaerat.'
+    },
+  ];
 
   before( () => {
     db = knex({
@@ -10,7 +24,7 @@ describe('Articles service object', () => {
       connection: process.env.TEST_DB_URL
     });
   });
-  
+
   describe('getAllArticles()', () => {
     it('resolves all articles from "blogful_articles" table', () => {
       //test that ArticleService.getAllArticles gets data from table
