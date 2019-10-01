690c62eb1872f54a51eb691f6e09b43c51bb3a2b
Steven Bull <stevenwbulljr@gmail.com>
2019-10-01 11:03:35 -0500

    add shoppingListPagination function based on user page input

--- src/drills.js
+++ src/drills.js
@@ -5,3 +5,30 @@ const knexInstance = knex({
   client: 'pg',
   connection: process.env.DB_URL
 });
+
+function userSearch(searchTerm) {
+  knexInstance
+    .select('*')
+    .from('shopping_list')
+    .where('name', 'ILIKE', `%${searchTerm}%`)
+    .then( res => console.log(res))
+    .finally( () => knexInstance.destroy());
+}
+
+/* userSearch('Wings'); */
+
+function shoppingListPagination(pageNumber) {
+  const productsPerPage = 6;
+  const offset = productsPerPage * (pageNumber - 1);
+
+  knexInstance
+    .select('*')
+    .from('shopping_list')
+    .limit(productsPerPage)
+    .offset(offset)
+    .then( res => console.log(res))
+    .finally( () => knexInstance.destroy());
+}
+
+shoppingListPagination(2);
+
