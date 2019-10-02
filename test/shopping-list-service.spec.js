cffbe2a2baf6de3ae657bc26b437505acb61b983
Steven Bull <stevenwbulljr@gmail.com>
2019-10-02 11:08:37 -0500

    finish empty table and populated table testing and function building

--- test/shopping-list-service.spec.js
+++ test/shopping-list-service.spec.js
@@ -6,23 +6,35 @@ describe('Shopping-List service object', () => {
 
   let testArticles = [
     {
+      id: 1,
       name: 'firstItem',
-      price: 12.22,
+      price: '12.22',
+      date_added: new Date('2029-01-22T16:28:32.615Z'),
+      checked: false,
       category: 'Main'
     },
     {
+      id: 2,
       name: 'secondItem',
-      price: 12.22,
+      price: '12.22',
+      date_added: new Date('2029-01-22T16:28:32.615Z'),
+      checked: false,
       category: 'Snack'
     },
     {
+      id: 3,
       name: 'thirdItem',
-      price: 12.22,
+      price: '12.22',
+      date_added: new Date('2029-01-22T16:28:32.615Z'),
+      checked: true,
       category: 'Lunch'
     },
     {
+      id: 4,
       name: 'fourthItem',
-      price: 12.22,
+      price: '12.22',
+      date_added: new Date('2029-01-22T16:28:32.615Z'),
+      checked: false,
       category: 'Breakfast'
     }
   ];
@@ -46,4 +58,26 @@ describe('Shopping-List service object', () => {
         .then( actual => expect(actual).to.eql([]) );
     });
   });
+
+  describe('When shopping_list is given data, it populates', () => {
+    before( () => {
+      return db.into('shopping_list').insert(testArticles);
+    });
+
+    it('getAllArticles() resolves all data into shopping_list table', () => {
+      return shoppingListService.getAllArticles(db)
+        .then( actual => {
+          expect(actual).to.eql(testArticles);
+          expect(actual).to.be.an('array');
+        });
+    });
+  });
+
+  describe('When update is made to shopping_list, new object is added', () => {
+
+  });
+
+  describe('When item is deleted, the item is removed from shopping_list', () => {
+    
+  })
 });
